ea4a42ad44485421a548c87d2e25af77b7c625b8
luizfiller <luiz.fsilva@sptech.school>
2023-03-28 10:17:23 -0300

    Add - módulo de Photos com Declarations e Exports

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhotoComponent } from './photo/photo.component';
+
+
+@NgModule({
+  declarations: [ PhotoComponent ],
+  exports: [PhotoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PhotosModule { }
