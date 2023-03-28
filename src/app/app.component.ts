3eb4063737b8e02ba1cd959603d24e258d972aee
luizfiller <luiz.fsilva@sptech.school>
2023-03-28 10:45:09 -0300

    Add - diretiva ngFor para prop photos + tirei % do width, pois quebrou responsividade Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
+  photos: Array<any> = [
+    {
+      url: 'https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg',
+      description: 'Imagem de leão',
+    },
+    {
+      url: 'https://cdn.pixabay.com/photo/2016/08/20/06/49/panthera-leo-1606964_1280.jpg',
+      description: 'Imagem de leoa',
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Suricato_perfil_Steam_2020.jpg',
+      description: 'Imagem de suricato',
+    },
+  ];
 }
